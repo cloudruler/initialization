c68b6d918845b496b5d84e0e008f5ae587ef3ec1
Brian Moore <brian.moore@cloudruler.io>
2021-03-05 05:28:10 +0000

    Updated Initialize-Azure.ps1

--- Initialize-Azure.ps1
+++ Initialize-Azure.ps1
@@ -1,38 +1,28 @@
-#Initialize the tenant
-
-New-AzADApplication -DisplayName "Terraform Cloud ARM Connector" -HomePage "https://app.terraform.io/" -IdentifierUris "https://app.terraform.io/"
-
-#New-AzADServicePrincipal -Role Contributor -Scope /subscriptions/$subscriptionId -DisplayName ""
-
-Secret                : System.Security.SecureString
-ServicePrincipalNames : {00000000-0000-0000-0000-000000000000, http://azure-powershell-05-22-2018-18-23-43}
-ApplicationId         : 00000000-0000-0000-0000-000000000000
-DisplayName           : azure-powershell-05-22-2018-18-23-43
-Id                    : 00000000-0000-0000-0000-000000000000
-Type                  : ServicePrincipal
-
-
-az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/SUBSCRIPTION_ID"
-Creating a role assignment under the scope of "/subscriptions/SUBSCRIPTION_ID"
-{
-  "appId": "00000000-0000-0000-0000-000000000000",
-  "displayName": "azure-cli-2017-06-05-10-41-15",
-  "name": "http://azure-cli-2017-06-05-10-41-15",
-  "password": "0000-0000-0000-0000-000000000000",
-  "tenant": "00000000-0000-0000-0000-000000000000"
-}
-
+[CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
     [string]
-    $Scope = "/subscriptions/00000000-0000-0000-0000-000000000000"
+    $Scope = "/subscriptions/00000000-0000-0000-0000-000000000000",
+    [Parameter(Mandatory=$true)]
+    [string]
+    $VaultName = "kvcloudrulerinfrastructure",
+    [Parameter(Mandatory=$true)]
+    [string]
+    $Location = "South Central US"
 )
 process {
 
-    az appconfig kv delete -n $AppConfigResourceName --key "*" --label "*" --yes
+    #Initialize the tenant
+    $rg = New-AzResourceGroup -Name "rg-infrastructure" -Location $Location
+    $keyVault = New-AzKeyVault -Name $VaultName -ResourceGroupName "rg-infrastructure" -Location $Location
 
-    #az appconfig kv export -n $AppConfigResourceName -d file  --path "./$Label.yml" --format yaml --label $Label --prefix $Prefix --separator '/' -y
+    $app = New-AzADApplication -DisplayName "Azure Infrastructure Automation" -HomePage "https://app.terraform.io/" -IdentifierUris "https://app.terraform.io/"
+    $servicePrincipal = New-AzADServicePrincipal -Role Contributor -Scope $Scope -DisplayName "Terraform Cloud ARM Connector" -ApplicationObject $app
+    $secret = Set-AzKeyVaultSecret -Name "terraform-cloud-arm-connector-secret" -VaultName $VaultName -SecretValue $servicePrincipal.Secret
 
-    #az appconfig kv import -n $AppConfigResourceName -s file  --path "./$Label.yml" --format yaml --label $Label --prefix $Prefix --separator '/' -y
+    Write-Host "Terraform Cloud ARM Connector:"
+    Write-Host "ObjectId: $($servicePrincipal.Id)"
+    Write-Host "ApplicationId: $($servicePrincipal.ApplicationId)"
+    Write-Host "Secret: $($secret.Name)"
 
 }
